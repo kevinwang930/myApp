384e88c296bf587f51c2c2a2262b0a3036f1c317
Kevin Wang <kevinwang09@foxmail.com>
2021-09-23 03:05:01 +0800

    add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,15 +25,21 @@ module.exports = {
         // A temporary hack related to IDE not resolving correct package.json
         'import/no-extraneous-dependencies': 'off',
         'import/no-dynamic-require': 'off',
+        'import/prefer-default-export': 'off',
         // '@typescript-eslint/no-inferrable-types': 2,
         'react/jsx-filename-extension': [1, {extensions: ['.js', '.jsx']}],
-        'react/react-in-jsx-scope': 'off',
+        'react/prop-types': 'off',
+        'react-hooks/exhaustive-deps': 'off',
+
         'global-require': 'off',
         'prefer-promise-reject-errors': 'off',
         camelcase: 'off',
         'no-restricted-syntax': 'off',
         radix: 'off',
         'consistent-return': 'off',
+        'no-param-reassign': 'off',
+        'react/jsx-props-no-spreading': 'off',
+        'no-use-before-define': 'off',
     },
     settings: {
         node: {

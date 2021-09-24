1d516849f4e797a7470cc727ba1eec37f0b7eb72
Kevin Wang <kevinwang09@foxmail.com>
2021-09-24 15:13:20 +0800

    add rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,7 @@ module.exports = {
     },
     rules: {
         'no-unused-vars': 'off',
+        'no-console': 'off',
         // A temporary hack related to IDE not resolving correct package.json
         'import/no-extraneous-dependencies': 'off',
         'import/no-dynamic-require': 'off',

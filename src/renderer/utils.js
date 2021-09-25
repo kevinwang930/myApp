af2cca37c9e24278a80a170aff8c016d4cacd82b
Kevin Wang <kevinwang09@foxmail.com>
2021-09-26 03:04:01 +0800

    add sqlite_initConect

---
+++ src/renderer/utils.js
@@ -0,0 +1,39 @@
+import {access, open} from 'fs/promises'
+import {constants, existsSync} from 'fs'
+import {
+    sqlite_getOrCreatePath,
+    sqlite_createLoadSchema,
+    sqlite_connect,
+} from './api/db'
+import {startPythonService} from '../bridges/utils'
+
+import {rendererLog} from './log'
+
+export async function checkFileWritable(path) {
+    try {
+        await access(path, constants.W_OK)
+    } catch (e) {
+        if (e.code === 'ENOENT') {
+            return true
+        }
+        return Promise.reject(e)
+    }
+
+    const fileHandler = await open(path, 'r+')
+    await fileHandler.close()
+    return true
+}
+
+export function sqlite_initConnect() {
+    const {sqliteFilePath, schemaPath} = sqlite_getOrCreatePath()
+    rendererLog.info(
+        `sqlite File path ${sqliteFilePath}, schema path ${schemaPath}`
+    )
+
+    if (!existsSync(sqliteFilePath)) {
+        sqlite_createLoadSchema(sqliteFilePath, schemaPath)
+        startPythonService()
+    }
+
+    sqlite_connect()
+}

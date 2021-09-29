f6588481dee9112bdff08a431d88c53b533a1e38
Kevin Wang <kevinwang09@foxmail.com>
2021-09-29 11:32:46 +0800

    add ipc setting and utils fuctions

--- src/bridges/settings.js
+++ src/bridges/settings.js
@@ -1,5 +1,18 @@
 import {ipcRenderer} from 'electron'
+import {existsSync, mkdirSync} from 'fs'
 
+export function getOutputPath() {
+    const outputPath = ipcRenderer.sendSync('get-output-path')
+    if (outputPath) {
+        if (!existsSync(outputPath)) {
+            mkdirSync(outputPath, {
+                recursive: true,
+            })
+        }
+        return outputPath
+    }
+    throw new Error('输出路径未定义')
+}
 export function getSqliteFilePath() {
     return ipcRenderer.sendSync('get-sqlite-file-path')
 }
@@ -14,4 +27,28 @@ export function getAppSqliteSchemaPath() {
 
 export function getGrpcProtoPath() {
     return ipcRenderer.sendSync('get-grpc-proto-path')
+}
+
+export function getTemplateExcelOrderPath() {
+    const templatePath = ipcRenderer.sendSync('get-template-excelOrder-path')
+    if (templatePath && existsSync(templatePath)) {
+        return templatePath
+    }
+    const appTemplatePath = getAppTemplateExcelOrderPath()
+    if (appTemplatePath && existsSync(appTemplatePath)) {
+        return appTemplatePath
+    }
+    throw new Error('订单excel模板不存在')
+}
+
+export function getAppTemplateExcelOrderPath() {
+    return ipcRenderer.sendSync('get-app-template-excelOrder-path')
+}
+
+export function resetSettingDefaults() {
+    ipcRenderer.invoke('store-reset')
+}
+
+export function openSettingFile() {
+    ipcRenderer.invoke('open-store-in-editor')
 }
--- src/bridges/utils.js
+++ src/bridges/utils.js
@@ -3,3 +3,7 @@ import {ipcRenderer} from 'electron'
 export function startPythonService() {
     ipcRenderer.invoke('start-pythonService')
 }
+
+export function restartPythonService() {
+    ipcRenderer.invoke('restart-pythonService')
+}
